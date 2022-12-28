414e9ccf0fa9f13a5d193b8a60727b84aec160f9
Cedric98 <23346008+Cedric98@users.noreply.github.com>
2022-12-28 23:58:08 +0100

    Add slot machine animation WIP

--- src/app/games/game-dice/game-dice.component.ts
+++ src/app/games/game-dice/game-dice.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { GameService } from '../game/game.service'
-
 @Component({
     selector: 'app-game-dice',
     templateUrl: './game-dice.component.html',
@@ -8,6 +7,13 @@ import { GameService } from '../game/game.service'
 })
 export class GameDiceComponent {
     public roll = '----'
+    public rollAnim = [false, false, false, false]
+    public slotNumbers = [...Array(10).keys()]
+    
+    @Input() slotsAnim1: { transitionDuration: string; transform: string }
+    @Input() slotsAnim2: { transitionDuration: string; transform: string }
+    @Input() slotsAnim3: { transitionDuration: string; transform: string }
+    @Input() slotsAnim4: { transitionDuration: string; transform: string }
 
     constructor(private gameService: GameService) {}
 
@@ -26,5 +32,57 @@ export class GameDiceComponent {
         } else {
             this.roll = rollNumber.toString()
         }
+        this.playAnim()
+    }
+
+    async playAnim(duration = 1): Promise<void> {
+        this.slotsAnim1 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(0)`
+        }
+
+        this.slotsAnim2 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(0)`
+        }
+
+        this.slotsAnim3 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(0)`
+        }
+
+        this.slotsAnim4 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(0)`
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, duration + 1 * 1000))
+
+        this.stopAnim()
+    }    
+
+    stopAnim(): void {
+        const duration = 1
+        
+        this.slotsAnim1 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(-${parseInt(this.roll[0]) * 30 + 300}px)`
+        }
+
+        this.slotsAnim2 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(-${parseInt(this.roll[1]) * 30 + 300}px)`
+        }
+
+        this.slotsAnim3 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(-${parseInt(this.roll[2]) * 30 + 300}px)`
+        }
+
+        this.slotsAnim4 = {
+            'transitionDuration': `${duration > 0 ? duration : 1}s`,
+            'transform': `translateY(-${parseInt(this.roll[3]) * 30 + 300}px)`
+        }
+        
     }
 }
